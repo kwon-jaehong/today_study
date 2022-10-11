89ac772b0885bc675161289ad34705ef1b05395f
mrjaehong <mrjaehong@naver.com>
2022-10-11 01:52:36 +0000

    fix

---
+++ 대회/dacon/관광데이터/2022_10_10_ml+hydra+kosim/inffer.py
@@ -0,0 +1,150 @@
+import random
+import torch
+import os
+import numpy as np
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from d_set import CustomDataset,MyCollate
+from torch.utils.data import DataLoader, random_split
+import torch.nn as nn
+from model import CustomModel
+from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
+from kobert_tokenizer import KoBERTTokenizer
+from torch.optim.lr_scheduler import LambdaLR
+import logging as log
+from torch import Tensor
+from torch import nn
+from torch.nn import functional as F
+from typing import Optional, Sequence
+from transformers import  AutoTokenizer
+
+# log.basicConfig(filename='./log2.txt', level=log.DEBUG)
+
+# writer = SummaryWriter('runs/g2_experiment_1')
+class FocalLoss(nn.modules.loss._WeightedLoss):
+    def __init__(self, weight=None, gamma=2,reduction='mean'):
+        super(FocalLoss, self).__init__(weight,reduction=reduction)
+        self.gamma = gamma
+        self.weight = weight #weight parameter will act as the alpha parameter to balance class weights
+
+    def forward(self, input, target):
+        if len(target.shape) ==0:
+            target = target.view(-1)
+            
+        ce_loss = F.cross_entropy(input, target,reduction=self.reduction,weight=self.weight)
+        pt = torch.exp(-ce_loss)
+        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
+        
+        return focal_loss
+    
+    
+CFG = {
+    'IMG_SIZE':224,
+    'EPOCHS':20,
+    'LEARNING_RATE':2e-5,
+    'BATCH_SIZE':6,
+    'SEED':41,
+    'TRAIN_RATE':0.9,
+    'NUM_WORKERS':4
+}
+def get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_training_steps, last_epoch=-1):
+    def lr_lambda(current_step: int):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        return max(
+            0.0, float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps))
+        )
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+seed_everything(CFG['SEED']) # Seed 고정
+
+
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+
+train_transform = A.Compose([
+                            A.Resize(CFG['IMG_SIZE'],CFG['IMG_SIZE']),
+                            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
+                            ToTensorV2()
+                            ])
+test_transform = A.Compose([
+                            A.Resize(CFG['IMG_SIZE'],CFG['IMG_SIZE']),
+                            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
+                            ToTensorV2()
+                            ])
+
+
+
+
+tokenizer =  AutoTokenizer.from_pretrained('BM-K/KoSimCSE-bert-multitask')
+
+
+train_all_dataset = CustomDataset('../train.csv',tokenizer,train_transform)
+label_info = train_all_dataset.label_3_level_2num
+
+
+
+## 데이터 로더
+pad_token_id = tokenizer.pad_token_id
+# batch_size: 6
+# image_size: 224
+# epochs: 20
+# num_worker: 4
+# lr: 0.00002
+# gpus: 2
+# train_data_rate: 0.9
+# train_path: "/data/mrjaehong/handwriting_gen/today_study/대회/dacon/관광데이터//train.csv"
+# test_path: "/data/mrjaehong/handwriting_gen/today_study/대회/dacon/관광데이터/test.csv"
+# main_model_drop_out_p: 0.3
+# image_encoder_drop_out_p: 0.1
+# image_token_size: 3
+# resnet_choice: 0
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = CustomModel(tokenizer,len(label_info),0.3,0.1,3,0).to(device)
+   
+
+model.load_state_dict(torch.load('./18.pth'))
+
+
+    
+test_dataset = CustomDataset('../test.csv',tokenizer,test_transform,infer=True)
+test_loader = DataLoader(test_dataset, batch_size = CFG['BATCH_SIZE'], num_workers = CFG['NUM_WORKERS'],pin_memory=True, collate_fn = MyCollate(pad_idx=pad_token_id,infer=True))
+    
+
+model.eval()
+
+model_preds = []
+count = 0
+with torch.no_grad():
+    for data_batch in test_loader:
+        count  += len(data_batch['image'])
+
+        img = data_batch['image']
+        text = data_batch['text']
+        mask = data_batch['mask']
+
+        img = img.float().to(device)
+        text = text.to(device)
+        mask = mask.to(device)
+        
+        model_pred = model(img, text,mask,device) 
+        model_preds += model_pred.argmax(1).detach().cpu().numpy().tolist()
+
+result = []
+for i in model_preds:
+    result.append(train_all_dataset.num2label_3_level[i])
+
+
+submit = pd.read_csv('../sample_submission.csv')
+submit['cat3'] = result
+
+submit.to_csv('./g2_best.csv', index=False)
---
+++ 대회/dacon/관광데이터/2022_10_11_bart/d_set.py
@@ -0,0 +1,108 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import cv2
+import torch.nn as nn
+import re
+import os
+
+    
+class CustomDataset(Dataset):
+    def __init__(self,csv_path,tokenizer,transforms,infer=False):
+        self.tokenizer = tokenizer
+        
+        self.all_df = pd.read_csv(csv_path)        
+        
+        
+        # 텍스트속 html 문법 제거
+        self.text_list = list(self.all_df['overview'])
+        for i,text in enumerate(self.text_list):
+            self.text_list[i] = self.cleanhtml(text)
+        
+        data_dir_name,_ = os.path.split(csv_path)
+        self.img_path_list = [os.path.join(data_dir_name,pt[2:]) for pt in list(self.all_df['img_path'])]
+        
+        
+        if infer==False:
+            self.label_level_list_3 = list(self.all_df['cat3'])
+            
+            set_level_list_3 = list(set(self.label_level_list_3))
+            
+            set_level_list_3.sort()            
+            
+            self.num2label_3_level = {i:label for i,label in enumerate(set_level_list_3)}
+            self.label_3_level_2num = {label:i for i,label in enumerate(set_level_list_3)}
+        
+         
+
+        self.transforms = transforms
+        self.infer = infer
+        
+    def __getitem__(self, index):
+
+        text = self.text_list[index]
+        text_token = self.tokenizer.encode(text)
+
+        ## 시작과 끝 토큰 떄기
+        text_token = text_token[1:-1]
+        # [2, 993, 6516, 5446, 5468, 2640, 6573, 6516, 4258, 7382, 6896, 3563, 7788, 3860, ...]
+        # list(self.tokenizer.get_vocab().keys())[932:935]
+        
+        text_len = len(text_token)
+        if text_len > 512: # 버트모델 maxlen 512 - 이미지 임베딩 토큰 3 - cls,sep 토큰 2 
+            text_token = text_token[:512]
+        
+        ## Image
+        img_path = self.img_path_list[index]
+        image = cv2.imread(img_path)
+        
+        if self.transforms is not None:
+            image = self.transforms(image=image)['image']        
+        # Label
+        if self.infer:
+            return image, torch.Tensor(text_token).view(-1).to(torch.long)
+        else:
+
+            label_3_level = self.label_3_level_2num[self.label_level_list_3[index]]
+            return image, torch.Tensor(text_token).view(-1).to(torch.long), torch.tensor([label_3_level],dtype=torch.long)
+
+    def __len__(self):
+        return len(self.all_df)
+    
+    def cleanhtml(self,raw_html):
+        cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+
+class MyCollate:
+    def __init__(self, pad_idx,infer=False):
+        ## pad idx 가져옴
+        self.pad_idx = pad_idx
+        self.infer = infer
+
+    def __call__(self, batch):
+        lens = [len(row[1]) for row in batch]
+        bsz, max_seq_len = len(batch), max(lens)
+        
+        mask_tensor = torch.zeros(bsz, max_seq_len, dtype=torch.long)
+        for i_batch, length in enumerate(lens):
+            mask_tensor[i_batch, :length] = 1
+        
+        
+        image = [item[0] for item in batch]  
+        text = [item[1] for item in batch]  
+        
+        text = nn.utils.rnn.pad_sequence(text, batch_first=True, padding_value = self.pad_idx) 
+        
+        if self.infer==False:
+            # label_level_1 = [item[2] for item in batch]
+            # label_level_2 = [item[3] for item in batch]
+            label_level_3 = [item[2] for item in batch]  
+            return {"image":torch.stack(image),"text":text,"mask":mask_tensor,"label_3":torch.stack(label_level_3).squeeze()}
+        else:
+            return {"image":torch.stack(image),"text":text,"mask":mask_tensor}
+    
+            
+            
+            
