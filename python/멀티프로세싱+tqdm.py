4a4dd9fa28f4a14d57e03ed8beb425310e86310f
mrjaehong <mrjaehong@naver.com>
2022-12-01 01:54:20 +0000

    fix

--- python/멀티프로세싱+tqdm.py
+++ python/멀티프로세싱+tqdm.py
@@ -2,19 +2,27 @@
 # from multiprocessing import Pool
 
 from tqdm import *
+from functools import partial
+from itertools import repeat
 
 from torch import multiprocessing as mp
 
-def f(x):
+
+
+def f(x,y):
+    global list_a
+    print("프로세스",mp.current_process()," == ",list_a)
     return x
 
 ## Pool에 아무것도 선언 하지않하면 CPU 최대로 설정이됨
-with mp.Pool() as p:
-    max = 10000
-    with tqdm(total=max) as pbar:
-        for _ in p.imap_unordered(f,range(0,max)):
-            pbar.update()
+def main():
+    global list_a
+    list_a = [1,2,3,4,5]
+    with mp.Pool() as p:
+        max = 10000
+        with tqdm(total=max) as pbar:
+            for _ in p.starmap(f,zip(range(0,max),range(0,max))):
+                pbar.update()
 
-
-
-print(1)
+if __name__ == "__main__":
+    main()
